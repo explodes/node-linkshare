879e43a8190be9646dcf7e2bd3b5628943857d55
Evan Leis <evan@employeerewards.com>
2011-08-24 09:27:29 -0600

    Added paymenthistory docs, bumped to version 0.0.2

--- src/services/paymenthistory.js
+++ src/services/paymenthistory.js
@@ -1,3 +1,20 @@
+/**
+ * http://helpcenter.linkshare.com/publisher/getattachment.php?data=MTExOXxVc2luZyBXZWIgU2VydmljZXMgZm9yIFBheW1lbnQgUmVwb3J0cy5wZGY%3D
+ * 
+ * Payment History Summary
+ * 
+ * This report provides the payment amount, the date issued, the currency, and
+ * other details on each consolidated payment you receive from LinkShare. It
+ * also includes the Payment ID, which you will need if you want to run the
+ * Advertiser Payments History and Payment Details Report.
+ *
+ * SAMPLE RESPONSE:
+ * 
+ * "Payment ID," "Date," "Payment Type," "Check Number," "Currency Code," Total Commission Amount Paid," "Payment Status" 
+ * "883463," "5/19/2011," "Domestic (US) Direct Deposit," "DAC883463," "USD," "568.24," "Issued"
+ *
+ */
+
 var helpers = require('./helpers')
    ,urls    = require('./urls')
    ;
